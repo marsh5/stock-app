ba9e10e8d46ce8d2d8850da72759696adc929957
marsh5 <brodiemarshall567@gmail.com>
2021-05-04 22:39:44 -0400

    remove any whitespace from ticker in handle add/remove function

--- client/src/components/FinancialPage.js
+++ client/src/components/FinancialPage.js
@@ -254,8 +254,11 @@ function FinancialPage() {
     const handleAddStock = async () => {
         console.log('handleAddStock clicked')
         console.log('ticker:', ticker)
-        const res = await portfolioServices.addStock(ticker.toUpperCase());
-        if(res === ticker.toUpperCase()){
+        //for the rare instances a ticker has a space in it
+        let tick = ticker.split(" ").join("")
+        console.log('tick', tick)
+        const res = await portfolioServices.addStock(tick.toUpperCase());
+        if(res === tick.toUpperCase()){
             toast.success(`${ticker.toUpperCase()} added to your portfolio!`)
             setIsPartOfPortfolio(true);
         }
@@ -266,10 +269,13 @@ function FinancialPage() {
     const handleRemoveStock = async () => {
         console.log('handleRemoveStock clicked')
         console.log('ticker:', ticker)
+
+        let tick = ticker.split(" ").join("")
+        console.log('tick', tick)
         
-        const res = await portfolioServices.removeStock(ticker.toUpperCase());
-        if(res === ticker.toUpperCase()){
-            toast.success(`${ticker.toUpperCase()} removed from your portfolio!`)
+        const res = await portfolioServices.removeStock(tick.toUpperCase());
+        if(res === tick.toUpperCase()){
+            toast.success(`${tick.toUpperCase()} removed from your portfolio!`)
             setIsPartOfPortfolio(false);
         }
         console.log('res', res);
